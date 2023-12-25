48280a4ab44cb3bf4b8563ebd2d06fa8f579f3cb
young beom <dudqja3674@gmail.com>
2023-12-25 19:05:15 +0900

    feat: TypeScript 마이그레이션 webpack dev 서버 설정
    .gitignore 수정, express uninstall

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,45 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".json", ".css"],
+  },
+  entry: "./src/index.ts", // 번들링 시작 위치
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "dist"), // 번들 결과물 위치
     filename: "bundle.js",
   },
+  module: {
+    rules: [
+      {
+        test: /[\.js]$/, // .js 에 한하여 babel-loader를 이용하여 transpiling
+        exclude: /node_modules/, // node_modules 내의 파일 제외
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: "./index.html", // 기존의 index.html 파일 위치
@@ -17,18 +51,8 @@ module.exports = {
     // ... (추가적인 플러그인 설정)
   ],
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/, // .js 파일에 대한 설정
-        exclude: /node_modules/, // node_modules 내의 파일 제외
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"], // ES6 및 JSX를 일반 JavaScript로 변환
-          },
-        },
-      },
-    ],
+  devServer: {
+    host: "localhost", // live-server host 및 port
+    port: 5500,
   },
 };
