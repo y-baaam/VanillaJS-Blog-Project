ba199c535144d82d207633887fc1ad8c4ae37ddc
young beom <dudqja3674@gmail.com>
2024-01-01 19:48:06 +0900

    feat: webpack 배포 환경 설정
    package.json scripts 변경,
    ts-loader devDependencies 로 이동, webpack common, dev, prod 생성
    방명록 페이지 제작 예정, blog > post 파일명 변경

--- src/components/Footer/footer.ts
+++ src/components/Footer/footer.ts
@@ -2,7 +2,7 @@ import styles from "./footer.css";
 function Footer() {
   return `
 <footer class=${styles.footer}>
-  <p>&copy; ${new Date().getFullYear()} 나의 블로그.</p>
+  <p>aaa</p>
 </footer>
 `;
 }
---
+++ src/views/guestBook.ts
@@ -0,0 +1,6 @@
+import Layout from "../components/Layout/layout";
+
+export default function GuestBook() {
+  const content = `<div>방명록 페이지</div>`;
+  return Layout(content);
+}
