18e091cede939d0fb9928561d51e89b29739b085
young beom <dudqja3674@gmail.com>
2024-01-21 01:59:27 +0900

    feat: utterances 기능 추가(test)
    layout, router type 수정
    string -> HTMLElement
    utterances html contents loaded 후에 실행하도록 함.

---
+++ src/components/utterances/utterances.ts
@@ -0,0 +1,15 @@
+export default function utterances(repo: string) {
+  const script = document.createElement("script");
+  script.src = "https://utteranc.es/client.js";
+  script.async = true;
+  script.setAttribute("repo", repo);
+  script.setAttribute("issue-term", "pathname");
+  script.setAttribute("label", "guest-book");
+  script.setAttribute("theme", "photon-dark");
+  script.crossOrigin = "anonymous";
+
+  const container = document.getElementById("utterances-container");
+  if (container) {
+    container.appendChild(script);
+  }
+}
