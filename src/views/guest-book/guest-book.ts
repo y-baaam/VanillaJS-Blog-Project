7140c3f517687f1015518e4bcfc32135db495b48
young beom <dudqja3674@gmail.com>
2024-01-21 15:33:20 +0900

    style: guest-book 주석 삭제

--- src/views/guest-book/guest-book.ts
+++ src/views/guest-book/guest-book.ts
@@ -12,14 +12,12 @@ export default async function GuestBook() {
     </div>
   `;
 
-  // 레이아웃 컨텐츠를 비동기적으로 로드합니다.
   const layoutContent = await Layout(content);
 
-  // appDiv에 layoutContent를 추가합니다.
   const appDiv = document.getElementById("app");
   if (appDiv) {
-    appDiv.innerHTML = ""; // 기존 내용을 제거합니다.
-    appDiv.appendChild(layoutContent); // 새로운 레이아웃 컨텐츠를 추가합니다.
+    appDiv.innerHTML = "";
+    appDiv.appendChild(layoutContent);
 
     // layoutContent가 DOM에 추가된 후에 utterances를 로드합니다.
     utterances("y-baaam/VanillaJS-Blog-Project");
