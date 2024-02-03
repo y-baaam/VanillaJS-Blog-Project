ab6829b7b9ffd488621159e2d9d10d116cf0c9fd
young beom <dudqja3674@gmail.com>
2024-02-03 12:37:06 +0900

    post: markdown html로 변환할 수 있도록 함
    content에 .md 파일 불러와서 html로 변환할 수 있도록 구성
    프론트메터도 사용할 수 있음

--- src/views/posts/post/post.ts
+++ src/views/posts/post/post.ts
@@ -1,26 +1,23 @@
 import Layout from "../../../components/Layout/layout";
-import styles from "./PostDetail.css";
+import styles from "./post.css";
 
-const fs = require("fs");
-const matter = require("gray-matter");
-const marked = require("marked");
+import matter from "gray-matter";
+import * as marked from "marked";
 import markContent from "../../../../content/example/index.md";
 
 export default function Post() {
-  const fileContent = fs.readFileSync(markContent);
-
   // gray-matter를 사용하여 프론트매터와 마크다운 본문을 분리합니다.
-  const { data: frontMatter, content: markdownContent } = matter(fileContent);
+  const { data: frontMatter, content: markdownContent } = matter(markContent);
 
   // marked를 사용하여 마크다운 본문을 HTML로 변환합니다.
-  const htmlContent = marked(markdownContent);
+  const htmlContent = marked.marked(markdownContent);
 
   console.log(frontMatter); // 프론트매터 메타데이터를 출력합니다.
   console.log(htmlContent); // 변환된 HTML을 출력합니다.
 
   const content = `
   <div class=${styles.postContainer}>
-    
+
   </div>`;
   return Layout(content);
 }
