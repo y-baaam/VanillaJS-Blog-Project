45466d721a67726a64e32a9a74dfa5d5f0c73a84
young beom <dudqja3674@gmail.com>
2024-02-19 20:45:39 +0900

    style: class 네이밍 변경
    BEM 방식으로 변경

--- src/views/posts/posts.ts
+++ src/views/posts/posts.ts
@@ -1,40 +1,31 @@
 import Layout from "../../components/Layout/layout";
 import styles from "./posts.css";
-import { getAllPosts } from "../../api/posts";
+import PostItem from "../../components/PostItem/postItem";
+import { getFeaturedPosts } from "../../api/posts";
 
-export default async function Post() {
-  const posts = await getAllPosts();
-  console.log(posts);
+export default async function Posts() {
+  const postItems = await PostItem();
+  const postsLength = (await getFeaturedPosts()).length;
 
   const content = `
-  <div class="${styles["posts"]}">
-    <div class="${styles["posts__header"]}">
-      <div class="${styles["posts__header--title"]}">All</div>
-      <div class="${styles["posts__header--num"]}">123 posts</div>
+  <div class=${styles["posts"]}>
+    <div class=${styles["posts__header"]}>
+      <div class=${styles["posts__header--title"]}>title</div>
+      <div class=${styles["posts__header--num"]}>${postsLength} posts</div>
     </div>
 
-    <div class="${styles["posts__body"]}">
-      <div class="${styles["posts__category--list"]}">
-        <div class="${styles["posts__category"]}">All</div>
-        <div class="${styles["posts__category"]}">category</div>
-        <div class="${styles["posts__category"]}">category</div>
-        <div class="${styles["posts__category"]}">category</div>
-        <div class="${styles["posts__category"]}">category</div>
-        <div class="${styles["posts__category"]}">category</div>
+    <div class=${styles["posts__body"]}>
+      <div class=${styles["posts__category--list"]}>
+        <div class=${styles["posts__category"]}>All</div>
+        <div class=${styles["posts__category"]}>category</div>
+        <div class=${styles["posts__category"]}>category</div>
+        <div class=${styles["posts__category"]}>category</div>
+        <div class=${styles["posts__category"]}>category</div>
+        <div class=${styles["posts__category"]}>category</div>
       </div>
-      <div class="${styles["posts__item"]}">
-        <div class="${styles["posts__item--title"]}">
-          제목
-        </div>
-        <p class="${styles["posts__item--text"]}">
-          contents
-        </p>
-        <div class="${styles["posts__item--bottom"]}">
-          <div class="${styles["posts__item--date"]}">date</div>
-          <div class="${styles["posts__item--category"]}">category</div>
-        </div>
-      </div>
+      ${postItems}
     </div>  
   </div>`;
+
   return Layout(content);
 }
