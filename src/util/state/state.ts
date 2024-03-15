dadd3692623e7f319e8486449d7fe6def0acaaae
young beom <dudqja3674@gmail.com>
2024-03-15 16:58:14 +0900

    feat: State
    observer pattern으로 변경

--- src/util/state/state.ts
+++ src/util/state/state.ts
@@ -1,10 +1,27 @@
-export function useState<T>(initialValue: T): [() => T, (newValue: T) => void] {
-  let value: T = initialValue;
+type Listener<T> = (value: T) => void;
 
-  const state = (): T => value;
-  const setState = (newValue: T): void => {
-    value = newValue;
-  };
+export class State<T> {
+  private value: T;
+  private listeners: Listener<T>[] = [];
 
-  return [state, setState];
+  constructor(initialValue: T) {
+    this.value = initialValue;
+  }
+
+  getValue(): T {
+    return this.value;
+  }
+
+  setValue(newValue: T): void {
+    this.value = newValue;
+    this.listeners.forEach((listener) => listener(newValue));
+  }
+
+  subscribe(listener: Listener<T>): void {
+    this.listeners.push(listener);
+  }
+
+  unsubscribe(listener: Listener<T>): void {
+    this.listeners = this.listeners.filter((l) => l !== listener);
+  }
 }
