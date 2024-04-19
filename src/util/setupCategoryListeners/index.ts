78f1a6e5664b8cd17a3ad17383f075ef6931b5f1
young beom <dudqja3674@gmail.com>
2024-04-20 01:36:29 +0900

    refactor: setupCategory 수정
    tailwind 마이그레이션 하면서 className 바꿔야되는 이슈 발생
    hasAttribute로 변경

--- src/util/setupCategoryListeners/index.ts
+++ src/util/setupCategoryListeners/index.ts
@@ -1,7 +1,6 @@
 import { State } from "../state";
 
 type setupCategory = {
-  styles: { [key: string]: string };
   categoryState: State<string | null>;
   render: () => void;
 };
@@ -11,25 +10,25 @@ type setupCategory = {
  * 지정된 카테고리에 해당하는 DOM 요소가 클릭되면 상태를 업데이트하고
  * 해당 카테고리에 맞게 화면을 다시 렌더링합니다.
  *
- * @param styles
  * @param categoryState 현재 선택된 카테고리의 상태를 관리하는 객체
  * @param render 카테고리가 변경되었을 때 호출되어 화면을 다시 렌더링하는 함수
  */
 
 export function setupCategoryListeners({
-  styles,
   categoryState,
   render,
 }: setupCategory) {
   document.addEventListener("click", (e) => {
-    let target = e.target as HTMLElement;
-    if (!target?.className?.includes(styles["posts__category"])) {
-      return;
+    let target = e.target as HTMLElement | null;
+    while (target && !target.hasAttribute("data-category")) {
+      target = target.parentElement;
     }
-    const category = target.getAttribute("data-category");
-    if (category) {
-      categoryState.setValue(category);
-      render();
+    if (target && target.hasAttribute("data-category")) {
+      const category = target.getAttribute("data-category");
+      if (category) {
+        categoryState.setValue(category);
+        render();
+      }
     }
   });
 }
