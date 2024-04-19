b3e9a5409e2394ee19f3c9114ce21fd3a5194491
young beom <dudqja3674@gmail.com>
2024-04-19 23:43:01 +0900

    chore: 폴더 구조 변경, css tailwind 마이그레이션

--- src/views/home/index.ts
+++ src/views/home/index.ts
@@ -1,20 +1,19 @@
 import Layout from "@comp/Layout";
-import styles from "./index.module.css";
 import { createRotatingText } from "@/util/rotatingText";
 
 export default function Home() {
   const rotatingWords = ["송영범", "나나나", "오오오", "징징징"];
   const content = `
-  <section class=${styles["home"]}>
+  <section>
     <div>안녕하세요</div>
     <div>
       <div class="h-10">
-        <div class="absolute top-1" id="rotatingText">aaa</div>
+        <div class="relative" id="rotatingText"></div>
       </div>
 
-      <h1 class="text-title-bold text-red-800">
+      <div class="text-title-bold">
         Hello world!
-      </h1>
+      </div>
     </div>
   </section>`;
   const layoutContent = Layout(content);
