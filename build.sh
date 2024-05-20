a89ac2b7166a240e04e5db5b4a5a0764e0c593eb
young beom <dudqja3674@gmail.com>
2024-05-20 15:46:49 +0900

    fix: build.sh 파일 rm publish/sitemap 제거

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
 pnpm run build && mv ./dist/index.html ./dist/index2.html && cp -r ./public/* ./dist/ &&
-rm ./dist/index.html && mv ./dist/index2.html ./dist/index.html && rm ./public/sitemap.xml
+rm ./dist/index.html && mv ./dist/index2.html ./dist/index.html
