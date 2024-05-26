a0aa79fd40b68ba1260e3079147a2f6885f5d26d
young beom <dudqja3674@gmail.com>
2024-05-26 13:56:38 +0900

    fix: build setting

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/sh
 
-pnpm run build  && rm ./public/sitemap.xml
-# && mv ./dist/index.html ./dist/index2.html && cp -r ./public/* ./dist/ &&
-# rm ./dist/index.html && mv ./dist/index2.html ./dist/index.html
+pnpm run build && mv ./dist/index.html ./dist/index2.html && cp -r ./public/* ./dist/ &&
+rm ./dist/index.html && mv ./dist/index2.html ./dist/index.html && rm ./public/sitemap.xml
