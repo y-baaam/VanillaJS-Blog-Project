c6ac02404b65a81c9400774474f0af58b75c2ac6
young beom <dudqja3674@gmail.com>
2024-05-26 19:27:41 +0900

    fix: img 경로 문제
    webpack 수정, .md 파일 수정

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -26,25 +26,7 @@ module.exports = merge(common, {
         { from: "public/content/posts", to: "content/posts" },
         { from: "public/sitemap.xml", to: "sitemap.xml" },
         { from: "public/robots.txt", to: "robots.txt" },
-        { from: "public/images", to: "images" },
       ],
     }),
   ],
-  output: {
-    path: path.resolve(__dirname, "../dist"),
-    publicPath: "/",
-  },
-
-  devServer: {
-    historyApiFallback: true,
-    static: [
-      {
-        directory: path.join(__dirname, "../public"),
-      },
-      {
-        directory: path.join(__dirname, "../public/content/posts"),
-      },
-    ],
-    port: 8080,
-  },
 });
